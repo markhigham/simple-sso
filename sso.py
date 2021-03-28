e2b54c621500933731159989dd5a2d2b6d187795
Mark Higham <mark.higham@gmail.com>
2021-03-28 14:53:56 +0100

    using test.user@example.com as default username

--- sso.py
+++ sso.py
@@ -4,7 +4,7 @@ from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-current_user = User()
+current_user = User(email="test.user@example.com", first_name="Test", last_name="User")
 
 
 @app.route("/o/authorize/")
