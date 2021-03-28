0e7e6a4f0ebdf7885a0dc76e0b31b535d228da8c
Mark Higham <mark.higham@gmail.com>
2021-03-28 14:49:09 +0100

    now you can change the user with http post - useful for selenium tests

--- sso.py
+++ sso.py
@@ -1,13 +1,59 @@
 from flask import jsonify
 from sso_user import User
-from flask import Flask
+from flask import Flask, request, redirect
+from uuid import uuid4
 
 app = Flask(__name__)
 
 current_user = User()
 
-@app.route('/o/authorize/')
+
+@app.route("/o/authorize/")
 def authorize():
-    
+
+    # app.logger.debug(request.args["redirect_uri"])
+
+    client_redirect_uri = request.args["redirect_uri"]
+
+    redirect_uri = f"{client_redirect_uri}?state=abcde&redirect_uri={client_redirect_uri}&code=any_old_code"
+
+    app.logger.debug(redirect_uri)
+    return redirect(redirect_uri, code=302)
+
+
+@app.route("/o/token/", methods=["POST"])
+def exchange_token():
+
+    result = {"access_token": current_user.id, "token_type": "Bearer"}
+    return jsonify(result)
+
+
+@app.route("/api/v1/user/me/")
+def get_user():
+
+    return jsonify(current_user.to_json())
+
+
+@app.route("/api/v1/user/set/", methods=["POST"])
+def set_user():
+
+    app.logger.debug(request.json)
+
+    new_user = request.json
+
+    if new_user.get("email"):
+        current_user.email = new_user["email"]
+
+    if new_user.get("email_user_id"):
+        current_user.email_user_id = new_user["email_user_id"]
+
+    if new_user.get("user_id"):
+        current_user.user_id = new_user["user_id"]
+
+    if new_user.get("first_name"):
+        current_user.first_name = new_user["first_name"]
+
+    if new_user.get("last_name"):
+        current_user.last_name = new_user["last_name"]
 
     return jsonify(current_user.to_json())
--- sso_user.py
+++ sso_user.py
@@ -6,30 +6,26 @@ fake = Faker(["en-GB"])
 
 
 class User:
-    email = ""
-    username = ""
-    first_name = ""
-    last_name = ""
-    email_user_id = ""
-    id = ""
-
-    def __init__(self, email: str = None, first_name: str = None, last_name: str = None) -> None:
-        
+    def __init__(
+        self, email: str = None, first_name: str = None, last_name: str = None
+    ) -> None:
 
         self.email = email if email else fake.email()
         self.first_name = first_name if first_name else fake.first_name()
         self.last_name = last_name if last_name else fake.last_name()
         self.email_user_id = self.email
-        self.username = self.email
         self.id = uuid.uuid4()
-
 
     def to_json(self) -> str:
         return {
             "email": self.email,
-            "username" : self.username,
+            "contact_email": self.email,
             "first_name": self.first_name,
-            "last_name" : self.last_name,
-            "email_user_id" : self.email_user_id,
-            "id": self.id
+            "last_name": self.last_name,
+            "email_user_id": self.email_user_id,
+            "id": self.id,
+            "related_emails": [],
+            "groups": [],
+            "permitted_applications": [],
+            "access_profiles": [],
         }
